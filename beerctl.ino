52a9f7f7230f4203d9c3c443c12a699ebf1cfdb5
lilia <liliakai@gmail.com>
2013-09-09 23:21:20 -0700

    Each sensor now has 2 targets - one for the first X minutes, and one thereafter, with 0 tolerance (i.e., any waver above or below should trigger a change in heater state

--- beerctl.ino
+++ beerctl.ino
@@ -1,8 +1,10 @@
 // threshold values in degrees F
-#define SENSOR_1_UPPER_THRESH 120.0
-#define SENSOR_2_UPPER_THRESH 120.0
-#define SENSOR_1_LOWER_THRESH 100.0
-#define SENSOR_2_LOWER_THRESH 100.0
+#define SENSOR_1_TARGET_1 150.0
+#define SENSOR_2_TARGET_1 160.0
+#define SENSOR_1_TARGET_2 160.0
+#define SENSOR_2_TARGET_2 170.0
+
+#define MILLIS_TIL_NEXT_TARGET 1800000 // 30 min = 1800000 ms
 
 // constants for conversion to degrees F
 #define ADC_SCALE 1.0
@@ -18,29 +20,40 @@
 #define SENSOR_2 A1
 #define HEATER 11
 
+float sensor1_target, sensor2_target;
+
 void setup() {
   Serial.begin(9600);
   pinMode(HEATER, OUTPUT);
   pinMode(SENSOR_1, INPUT);
   pinMode(SENSOR_2, INPUT);
+  sensor1_target = SENSOR_1_TARGET_1;
+  sensor2_target = SENSOR_2_TARGET_1;
 }
 
 void loop() {
   float sensor1 = read(SENSOR_1);
   float sensor2 = read(SENSOR_2);
 
-  if (valid_gt(sensor1, SENSOR_1_UPPER_THRESH) ||
-      valid_gt(sensor2, SENSOR_2_UPPER_THRESH) ) {
+  if (valid_gt(sensor1, sensor1_target) ||
+      valid_gt(sensor2, sensor2_target) ) {
     digitalWrite(HEATER, LOW); // turn heater off
   }
 
-  if (valid_lt(sensor1, SENSOR_1_LOWER_THRESH) ||
-      valid_lt(sensor2, SENSOR_2_LOWER_THRESH) ) {
+  if (valid_lt(sensor1, sensor1_target) ||
+      valid_lt(sensor2, sensor2_target) ) {
     digitalWrite(HEATER, HIGH); // turn heater on
   }
+
   Serial.print(sensor1);
   Serial.print(sensor2);
-  Serial.println('----');
+  Serial.println("----");
+
+  if (millis() > MILLIS_TIL_NEXT_TARGET) {
+    sensor1_target = SENSOR_1_TARGET_2;
+    sensor2_target = SENSOR_2_TARGET_2;
+    Serial.println("Switched target temperature!");
+  }
 }
 
 float read(int sensor_pin) {
